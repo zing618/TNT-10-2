71fa7cbb60c826dffb4cfb4408c2c23419187f96
zing618 <71222241+zing618@users.noreply.github.com>
2020-12-23 13:43:00 +0800

    Update qq_read.py

--- scripts/qq_read.py
+++ scripts/qq_read.py
@@ -668,7 +668,7 @@ def qq_read():
                 notify.send(title=title, content=content, notify_mode=notify_mode)
             elif not qq_read_config['notify']:
                 print('未进行消息推送，原因：未设置消息推送。如需发送消息推送，请确保配置文件的对应的脚本任务中，参数notify的值为true\n')
-            elif not beijing_datetime.hour == 22:
+            elif not beijing_datetime.hour == 8:
                 print('未进行消息推送，原因：没到对应的推送时间点\n')
             else:
                 print('未在规定的时间范围内\n')
